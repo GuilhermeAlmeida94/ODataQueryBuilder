e51245fd5d7ce03f93cbe153516decb598d40735
Guiphb <gui.phb@gmail.com>
2020-11-29 16:05:46 -0300

    Adding file not tracked.

---
+++ src/enums/comparisonOperator.ts
@@ -0,0 +1,8 @@
+export enum ComparisonOperator {
+    Equals = <any>"eq",
+    Less = <any>"lt",
+    Greater = <any>"gt",
+    GreaterOrEqual = <any>"ge",
+    LessOrEqual = <any>"le",
+    NotEqual = <any>"ne"
+}
